dad84ffc198175e72ac8b8efa898116dbe761f9e
JeChangYub <starhaja1@gmail.com>
2011-06-03 14:34:24 +0900

    First Commit

---
+++ MobileRobots_JE_Android/src/kr/ac/uos/je/MobileRobotsAndroidVerMainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.uos.je;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileRobotsAndroidVerMainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
